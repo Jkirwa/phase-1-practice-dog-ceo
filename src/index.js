84c7b285c808fcb389128d78b454b7ee25ee5641
Daisy Jepleting <jepledaisy@gmail.com>
2022-06-25 15:43:03 +0300

    Done

--- src/index.js
+++ src/index.js
@@ -19,7 +19,9 @@ function fetchBreed(){
     fetch ('https://dog.ceo/api/breeds/list/all')
     .then((response)=>response .json())
     .then(dogs => {
-        console.log(dogs);
+        breeds = Object.keys(dogs.message);
+        updateBreedList(breeds);
+        addBreedSelectListener();
         
     });
 }
@@ -31,11 +33,41 @@ function fetchBreed(){
     dogsCont.appendChild(ourImage);
 }
 
-function  loadBreedToDom(pic){
 
-    let dogBreed = document.getElementById('dog-breeds')
-    let breedImages = document.createElement('ul')
+function updateBreedList(breeds) {
+    let ul = document.querySelector('#dog-breeds');
+    removeChildren(ul);
+    breeds.forEach(breed => addBreed(breed));
+  }
 
-    breedImages.src = pic
-    dogBreed.append(breedImages)
-}
+  function removeChildren(element) {
+    let child = element.lastElementChild;
+    while (child) {
+      element.removeChild(child);
+      child = element.lastElementChild;
+    }
+  }
+  
+  function selectBreedsStartingWith(letter) {
+    updateBreedList(breeds.filter(breed => breed.startsWith(letter)));
+  }
+  
+  function addBreedSelectListener() {
+    let breedDropdown = document.querySelector('#breed-dropdown');
+    breedDropdown.addEventListener('change', function (event) {
+      selectBreedsStartingWith(event.target.value);
+    });
+  }
+  
+  function addBreed(breed) {
+    let ul = document.querySelector('#dog-breeds');
+    let li = document.createElement('li');
+    li.innerText = breed;
+    li.style.cursor = 'pointer';
+    ul.appendChild(li);
+    li.addEventListener('click', updateColor);
+  }
+  
+  function updateColor(event) {
+    event.target.style.color = 'blue';
+  }
