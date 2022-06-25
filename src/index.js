2ff7b72e13f8a7d5e498ed5412ea4b9d251c95c6
Daisy Jepleting <jepledaisy@gmail.com>
2022-06-25 15:31:53 +0300

    updating

--- src/index.js
+++ src/index.js
@@ -1,1 +1,41 @@
 console.log('%c HI', 'color: firebrick')
+
+document.addEventListener("DOMContentLoaded",function(){
+    fetchDogName();
+    fetchBreed();
+})
+
+function fetchDogName(){
+    fetch ("https://dog.ceo/api/breeds/image/random/4")
+    .then((response)=>response .json())
+    .then(data => {
+        console.log(data);
+        let images=data.message;
+      images.forEach(image => loadImagestoDom(image))
+    });
+}
+
+function fetchBreed(){
+    fetch ('https://dog.ceo/api/breeds/list/all')
+    .then((response)=>response .json())
+    .then(dogs => {
+        console.log(dogs);
+        
+    });
+}
+
+    function loadImagestoDom(image){
+    let dogsCont=document.getElementById("dog-image-container")
+    let ourImage=document.createElement("img");
+    ourImage.src=image;
+    dogsCont.appendChild(ourImage);
+}
+
+function  loadBreedToDom(pic){
+
+    let dogBreed = document.getElementById('dog-breeds')
+    let breedImages = document.createElement('ul')
+
+    breedImages.src = pic
+    dogBreed.append(breedImages)
+}
